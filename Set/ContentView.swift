51b1d9e09cb593f7977c5b48f2b662b614cb9081
slava bily <slavabily@me.com>
2022-10-20 14:14:33 +0300

    Initial Commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by slava bily on 20.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by slava bily on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
